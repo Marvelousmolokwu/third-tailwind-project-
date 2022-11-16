f94ba290280ed465780f9bbab0301ec5b43bb5ee
Marvelousmolokwu <qmolokwu@gmail.com>
2022-11-16 13:00:35 +0100

    changes made to make site more responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         hero: "url('/images/bg-header-mobile.png')",
-        "hero-desktop": "url('/images/bg-header-desktop.png')",
+        hero_desktop: "url('/images/bg-header-desktop.png')",
       },
       colors: {
         "strong-Cyan": "hsl(171, 66%, 44%)",
@@ -14,7 +14,15 @@ module.exports = {
         "Grayish-Blue": "hsl(201, 11%, 66%)",
       },
       fontFamily: {
-        body:["Bai Jamjuree"],
+        body: ["Bai Jamjuree"],
+      },
+      screens: {
+        df: "280px",
+        sm: "375px",
+        md: "768px",
+        lg: "910px",
+        xl: "1024px",
+        xxl: "1400px",
       },
     },
   },
